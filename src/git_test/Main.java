3ae598c184e7beea45a4e45aa440b5984e70aa8c
Heexoo <hisu138@gmail.com>
2022-09-26 17:04:19 +0900

    Main.java 수정#4

--- src/git_test/Main.java
+++ src/git_test/Main.java
@@ -2,8 +2,13 @@ package git_test;
 
 public class Main {
 
+	static void say(){
+		System.out.println("main");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		say();
 		Hello hello = new Hello();
 		hello.say("hello world");
 	}
